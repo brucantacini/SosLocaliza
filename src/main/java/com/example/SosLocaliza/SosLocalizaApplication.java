185ece0c91d6b04d073196ee0f082611e3461e32
Bruno Cantacini <brunocantacini.ti@gmail.com>
2025-10-09 20:31:15 -0300

    init: inicializando projeto java SOS localiza

---
+++ src/main/java/com/example/SosLocaliza/SosLocalizaApplication.java
@@ -0,0 +1,13 @@
+package com.example.SosLocaliza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosLocalizaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SosLocalizaApplication.class, args);
+	}
+
+}
